c7e3fe20d4efa926a36abf8a9e6a3a22e5119ae4
Malan-Tai <malantai99@gmail.com>
2025-01-28 22:58:29 +0100

    Smooth horizontal layout

---
+++ Assets/Scripts/UI/SmoothHorizontalLayoutGroup.cs
@@ -0,0 +1,189 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class SmoothHorizontalLayoutGroup : HorizontalLayoutGroup
+    {
+        public override void SetLayoutHorizontal()
+        {
+            SetChildrenAlongAxis(0, false);
+        }
+
+        private void SetAnchoredPosition(RectTransform rect, Vector2 target, int axis)
+        {
+            Vector2 current = rect.anchoredPosition;
+            if (Application.isPlaying && axis == 0)
+            {
+                Vector2 immediate = new Vector2(current.x, target.y);
+                rect.anchoredPosition = immediate;
+                rect.DOAnchorPosX(target.x, 0.5f);
+            }
+            else
+            {
+                rect.anchoredPosition = target;
+            }
+        }
+
+        // Copy paste of HorizontalOrVerticalLayoutGroup
+        private new void SetChildrenAlongAxis(int axis, bool isVertical)
+        {
+            float size = rectTransform.rect.size[axis];
+            bool controlSize = (axis == 0 ? m_ChildControlWidth : m_ChildControlHeight);
+            bool useScale = (axis == 0 ? m_ChildScaleWidth : m_ChildScaleHeight);
+            bool childForceExpandSize = (axis == 0 ? m_ChildForceExpandWidth : m_ChildForceExpandHeight);
+            float alignmentOnAxis = GetAlignmentOnAxis(axis);
+
+            bool alongOtherAxis = (isVertical ^ (axis == 1));
+            int startIndex = m_ReverseArrangement ? rectChildren.Count - 1 : 0;
+            int endIndex = m_ReverseArrangement ? 0 : rectChildren.Count;
+            int increment = m_ReverseArrangement ? -1 : 1;
+            if (alongOtherAxis)
+            {
+                float innerSize = size - (axis == 0 ? padding.horizontal : padding.vertical);
+
+                for (int i = startIndex; m_ReverseArrangement ? i >= endIndex : i < endIndex; i += increment)
+                {
+                    RectTransform child = rectChildren[i];
+                    float min, preferred, flexible;
+                    GetChildSizes(child, axis, controlSize, childForceExpandSize, out min, out preferred, out flexible);
+                    float scaleFactor = useScale ? child.localScale[axis] : 1f;
+
+                    float requiredSpace = Mathf.Clamp(innerSize, min, flexible > 0 ? size : preferred);
+                    float startOffset = GetStartOffset(axis, requiredSpace * scaleFactor);
+                    if (controlSize)
+                    {
+                        SetChildAlongAxisWithScale(child, axis, startOffset, requiredSpace, scaleFactor);
+                    }
+                    else
+                    {
+                        float offsetInCell = (requiredSpace - child.sizeDelta[axis]) * alignmentOnAxis;
+                        SetChildAlongAxisWithScale(child, axis, startOffset + offsetInCell, scaleFactor);
+                    }
+                }
+            }
+            else
+            {
+                float pos = (axis == 0 ? padding.left : padding.top);
+                float itemFlexibleMultiplier = 0;
+                float surplusSpace = size - GetTotalPreferredSize(axis);
+
+                if (surplusSpace > 0)
+                {
+                    if (GetTotalFlexibleSize(axis) == 0)
+                        pos = GetStartOffset(axis,
+                            GetTotalPreferredSize(axis) - (axis == 0 ? padding.horizontal : padding.vertical));
+                    else if (GetTotalFlexibleSize(axis) > 0)
+                        itemFlexibleMultiplier = surplusSpace / GetTotalFlexibleSize(axis);
+                }
+
+                float minMaxLerp = 0;
+                if (GetTotalMinSize(axis) != GetTotalPreferredSize(axis))
+                    minMaxLerp = Mathf.Clamp01((size - GetTotalMinSize(axis)) /
+                                               (GetTotalPreferredSize(axis) - GetTotalMinSize(axis)));
+
+                for (int i = startIndex; m_ReverseArrangement ? i >= endIndex : i < endIndex; i += increment)
+                {
+                    RectTransform child = rectChildren[i];
+                    float min, preferred, flexible;
+                    GetChildSizes(child, axis, controlSize, childForceExpandSize, out min, out preferred, out flexible);
+                    float scaleFactor = useScale ? child.localScale[axis] : 1f;
+
+                    float childSize = Mathf.Lerp(min, preferred, minMaxLerp);
+                    childSize += flexible * itemFlexibleMultiplier;
+                    if (controlSize)
+                    {
+                        SetChildAlongAxisWithScale(child, axis, pos, childSize, scaleFactor);
+                    }
+                    else
+                    {
+                        float offsetInCell = (childSize - child.sizeDelta[axis]) * alignmentOnAxis;
+                        SetChildAlongAxisWithScale(child, axis, pos + offsetInCell, scaleFactor);
+                    }
+
+                    pos += childSize * scaleFactor + spacing;
+                }
+            }
+        }
+
+        // Copy paste of HorizontalOrVerticalLayoutGroup
+        private new void SetChildAlongAxisWithScale(RectTransform rect, int axis, float pos, float scaleFactor)
+        {
+            if (rect == null)
+                return;
+
+            m_Tracker.Add(this, rect,
+                DrivenTransformProperties.Anchors |
+                (axis == 0
+                    ? DrivenTransformProperties.AnchoredPositionX
+                    : DrivenTransformProperties.AnchoredPositionY));
+
+            // Inlined rect.SetInsetAndSizeFromParentEdge(...) and refactored code in order to multiply desired size by scaleFactor.
+            // sizeDelta must stay the same but the size used in the calculation of the position must be scaled by the scaleFactor.
+
+            rect.anchorMin = Vector2.up;
+            rect.anchorMax = Vector2.up;
+
+            Vector2 anchoredPosition = rect.anchoredPosition;
+            anchoredPosition[axis] = (axis == 0)
+                ? (pos + rect.sizeDelta[axis] * rect.pivot[axis] * scaleFactor)
+                : (-pos - rect.sizeDelta[axis] * (1f - rect.pivot[axis]) * scaleFactor);
+            SetAnchoredPosition(rect, anchoredPosition, axis);
+        }
+
+        // Copy paste of HorizontalOrVerticalLayoutGroup
+        private new void SetChildAlongAxisWithScale(RectTransform rect, int axis, float pos, float size,
+            float scaleFactor)
+        {
+            if (rect == null)
+                return;
+
+            m_Tracker.Add(this, rect,
+                DrivenTransformProperties.Anchors |
+                (axis == 0
+                    ? (DrivenTransformProperties.AnchoredPositionX | DrivenTransformProperties.SizeDeltaX)
+                    : (DrivenTransformProperties.AnchoredPositionY | DrivenTransformProperties.SizeDeltaY)
+                )
+            );
+
+            // Inlined rect.SetInsetAndSizeFromParentEdge(...) and refactored code in order to multiply desired size by scaleFactor.
+            // sizeDelta must stay the same but the size used in the calculation of the position must be scaled by the scaleFactor.
+
+            rect.anchorMin = Vector2.up;
+            rect.anchorMax = Vector2.up;
+
+            Vector2 sizeDelta = rect.sizeDelta;
+            sizeDelta[axis] = size;
+            rect.sizeDelta = sizeDelta;
+
+            Vector2 anchoredPosition = rect.anchoredPosition;
+            anchoredPosition[axis] = (axis == 0)
+                ? (pos + size * rect.pivot[axis] * scaleFactor)
+                : (-pos - size * (1f - rect.pivot[axis]) * scaleFactor);
+            SetAnchoredPosition(rect, anchoredPosition, axis);
+        }
+
+        // Copy paste of HorizontalOrVerticalLayoutGroup
+        private void GetChildSizes(RectTransform child, int axis, bool controlSize, bool childForceExpand,
+            out float min, out float preferred, out float flexible)
+        {
+            if (!controlSize)
+            {
+                min = child.sizeDelta[axis];
+                preferred = min;
+                flexible = 0;
+            }
+            else
+            {
+                min = LayoutUtility.GetMinSize(child, axis);
+                preferred = LayoutUtility.GetPreferredSize(child, axis);
+                flexible = LayoutUtility.GetFlexibleSize(child, axis);
+            }
+
+            if (childForceExpand)
+                flexible = Mathf.Max(flexible, 1);
+        }
+    }
+}
